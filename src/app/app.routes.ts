20d4847e13b2113144196d6148677636e96df3a6
fatmatawfeek <fatmatawfeek97@gmail.com>
2024-11-22 02:58:48 +0200

    create about component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -7,5 +8,10 @@ export const routes: Routes = [
     path: 'home',
     component: HomeComponent,
     title: 'Home',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About',
   },
 ];
